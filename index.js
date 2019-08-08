32062d7ceb798275a6740b61431c69511e32df44
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-08 13:28:25 -0600

    ADded dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// code away!
+require("dotenv").config();
+
 const server = require("./server");
 
-const port = 8050;
+const port = process.env.PORT || 8050;
 
 server.listen(port, () => {
   console.log(`API is up and running on port ${port}`);
